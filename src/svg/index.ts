ec41dacadd5149eef559c64f59bf4f9b5a7ca729
Nika Nozadze <nikanozadze1010@gmail.com>
2023-08-16 00:44:36 +0400

    feat: create scg components

---
+++ src/svg/index.ts
@@ -0,0 +1,5 @@
+export { default as DownArrow } from "./DownArrow";
+export { default as UpArrow } from "./UpArrow";
+export { default as Close } from "./Close";
+export { default as Logo } from "./Logo";
+export { default as Settings } from "./Settings";
