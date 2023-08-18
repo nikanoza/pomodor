2fbc3d960c807f0a8ed8aa39f01b5953e4e254e3
Nika Nozadze <nikanozadze1010@gmail.com>
2023-08-19 02:40:51 +0400

    feat: create settings type

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import { useState } from "react";
 import { Modal, Panel, Session } from "./components";
 import { Logo, Settings } from "./svg";
+import { SettingsType } from "./types";
 
 function App() {
   const [showModal, setShowModal] = useState<boolean>(false);
+  const [settings, setSettings] = useState<SettingsType>({
+    pomodoro: 25,
+    longBreak: 15,
+    shortBreak: 5,
+    font: "kumbh",
+    color: "semi-red",
+  });
+
+  const updateSettings = () => {};
+
   return (
     <div className="w-full h-full flex flex-col items-center bg-semi-blu pt-8 pb-12">
       <Logo />
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,3 +1,13 @@
 export interface SvgElementType {
   onClick?: () => void;
 }
+
+export interface SettingsType {
+  pomodoro: number;
+  longBreak: number;
+  shortBreak: number;
+  font: "kumbh" | "roboto" | "mono";
+  color: "semi-red" | "sky" | "violet";
+}
+
+export type SettingsTypeKey = keyof SettingsType;
