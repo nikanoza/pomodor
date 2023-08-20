4f9c70b42a1df0409135c7be89d41e9fa0411ccc
Nika Nozadze <nikanozadze1010@gmail.com>
2023-08-20 13:21:12 +0400

    feat: done with timer functionality

--- src/components/Session.tsx
+++ src/components/Session.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { SessionType, SettingsType, TimeState } from "../types";
 
 type PropsType = {
@@ -17,8 +17,11 @@ const Session: React.FC<PropsType> = ({ settings, session }) => {
 
   const startTimer = () => {
     const time = setInterval(() => {
-      const clone = { ...times };
-      clone[session] = clone[session] - 1;
+      setTimes((prevTimes) => {
+        const clone = { ...prevTimes };
+        clone[session] = clone[session] - 1;
+        return clone;
+      });
     }, 1000);
 
     setTimer(time);
@@ -29,16 +32,43 @@ const Session: React.FC<PropsType> = ({ settings, session }) => {
     setTimer(null);
   };
 
+  useEffect(() => {
+    if (timer) {
+      pauseTimer();
+      startTimer();
+    }
+  }, [session]);
+
+  const displayTime = () => {
+    let minutes: string | number = Math.floor(times[session] / 60);
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+
+    let seconds: string | number = times[session] % 60;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+    return `${minutes}:${seconds}`;
+  };
+
+  const intervalStateHandler = () => {
+    if (timer) {
+      pauseTimer();
+    } else {
+      startTimer();
+    }
+  };
+
   return (
     <div className="mt-12 w-75 h-75 rounded-full bg-session flex justify-center items-center">
       <div className="w-[267.805px] h-[267.805px] rounded-full bg-dark-blu flex justify-center items-center">
         <div className="w-[238.049px] h-[238.049px] rounded-full border-[10px] border-semi-red flex flex-col justify-center items-center">
-          <h1 className="text-semi-grey text-[80px] font-bold tracking-[-4px] kumbh">
-            17:59
+          <h1 className="text-semi-grey text-[60px] font-bold tracking-[-4px]">
+            {displayTime()}
           </h1>
-          <h3 className="text-semi-grey text-sm font-bold tracking-[13.125px] kumbh">
-            PLAY
-          </h3>
+          <button
+            className={`text-semi-grey text-sm font-bold border-none tracking-[13.125px] ${settings.font}`}
+            onClick={intervalStateHandler}
+          >
+            {timer ? "PAUSE" : "PLAY"}
+          </button>
         </div>
       </div>
     </div>
